0cc3ef7df1826d451c4fe62d167987321284e22c
Christoph Hartmann <c.hartmann@carepayinternational.com>
2020-07-25 19:33:02 +0200

    added two new programs

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -28,9 +28,13 @@ int ix=0;
 bool state=0;
 bool button_state=0;
 int state2=0;
+const byte divisors[] = {1, 2, 3, 4, 5, 6, 10}; // based on NUM_LEDS
+const byte n_divisors = 7;
 const int n_states = 10;
 int states[n_states];
+int states2[n_states];
 bool bool_states[n_states];
+const int change_const = 2;
 
 void setup()
 {
@@ -128,13 +132,13 @@ void step_1(){
     state2 = value[5];
   } else{
     if(state==0){
-      ix = ix + (255-value[3])/(value[4]/2 + 1);
+      ix = ix + (255-value[4])/(value[3]/2 + 1);
       if(ix>=255){
         ix = 255;
         state = 1;
       };
     } else {
-      ix = ix - (255-value[3])/(value[4]/2 + 1);
+      ix = ix - (255-value[4])/(value[3]/2 + 1);
       if(ix<=value[3]){
         ix = value[3];
         state = 0;
@@ -180,7 +184,7 @@ void step_2(){
         bool_states[i] = 0;
       };
     };    
-    leds[states[i]] = CHSV((value[2]+(255/n_states)*(i-(i%2)))%255, 255, 255);
+    leds[states[i]] = CHSV((value[2]+(value[5]/n_states)*(i-(i%2)))%255, 255, 255);
   }  
   fadeall(value[4]);
 
@@ -198,9 +202,35 @@ void step_2(){
 
 void step_3(){
   calc_button2_on();
-  fadeall(value[4]);
-  leds[ix] = CHSV(value[2], 255, 255);
-  delay(value[3]/4);
+  if (!button_state){
+    fadeall(100);
+  };
+  int dist = divisors[int(float(value[4])/254.0*(n_divisors-1))];
+  for (int i=0; i<NUM_LEDS; i = i+(dist*2)){
+    for (int j=0; j<(dist*2) && ((i+j)<NUM_LEDS); j++){
+      state2 = (i+j+ix);
+      if (state2>=NUM_LEDS){state2=state2%NUM_LEDS;};
+      if (state2<0){state2=state2+NUM_LEDS;};
+      if (j<dist){
+        leds[state2] = CHSV(value[2], 255, 255);
+      }
+      if (j>=dist && button_state){
+        leds[state2] = CHSV(value[5], 255, 255);
+      };
+    }
+  }
+  if (abs(ix)>=(dist*2)){
+    ix = 0;
+  };
+  int tmp = (value[3]-128)/2;
+  if (tmp>=0){
+    ix = ix+1;
+    delay(-(tmp-64));
+  }
+  else{
+    ix = ix-1;
+    delay(tmp+64);
+  };
 }
 
 void step_4(){
@@ -215,8 +245,8 @@ void step_4(){
       leds[NUM_LEDS/2 - i] = CHSV(value[2], 255, 255);
     } else{
       if (button_state){
-        leds[NUM_LEDS/2 + i] = CHSV(value[5], 255, 100);
-        leds[NUM_LEDS/2 - i] = CHSV(value[5], 255, 100);
+        leds[NUM_LEDS/2 + i] = CHSV(value[5], 255, 150);
+        leds[NUM_LEDS/2 - i] = CHSV(value[5], 255, 150);
       };
     };
   }
@@ -232,6 +262,44 @@ void step_4(){
   else{
     ix = ix-1;
   };
+  delay(value[3]/4);
+}
+
+void step_5(){
+  if (value[1]==1){
+    if (button2_on==0){
+      if (state==0){
+        state2 = (state2+1)%(n_states-1);
+        bool_states[state2] = random(1);
+        states[state2] = random(NUM_LEDS-1);
+        states2[state2] = random(255-1);
+        button2_on = 1;
+      };
+    };
+  } else{
+    button2_on = 0;
+  };
+
+  for(int i=0; i<state2; i++){
+    if(bool_states[i]==0){
+      states[i] = states[i]+1;
+      if((states[i] >= NUM_LEDS)){
+        states[i] = NUM_LEDS-1;
+        bool_states[i] = 1;
+      };
+    } else {
+      states[i] = states[i]-1;
+      if(states[i]<=0){
+        states[i] = 0;
+        bool_states[i] = 0;
+      };
+    };
+    if(value[5]>random(1000)){bool_states[i] = !bool_states[i];}
+    states2[i] = (states2[i]+value[2]/30)%255;
+    leds[states[i]] = CHSV(states2[i], 255, 255);
+  }
+  fadeall(value[4]);
+
   delay(value[3]/4);
 }
 
@@ -284,10 +352,13 @@ void loop() {
       step_2();
       break;
     case 3:
-      step_4();
+      step_3();
       break;
     case 4:
-      step_3();
+      step_4();
+      break;
+    case 5:
+      step_5();
       break;
   }
   FastLED.show(); 
