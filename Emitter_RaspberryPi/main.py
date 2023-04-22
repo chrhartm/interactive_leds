baf2e505cc496de0cb26a3f806c7d5e871dde08c
Christoph Hartmann <christophartmann@gmail.com>
2023-04-22 16:24:06 +0100

    Changed to Stadia bluetooth controller

--- Emitter_RaspberryPi/main.py
+++ Emitter_RaspberryPi/main.py
@@ -3,9 +3,12 @@ import numpy as np
 import aubio
 from collections import deque
 from RF24 import RF24
-import pygame   
+import pygame
+import time
+import imp
+import subprocess
 
-# Global variables
+# Global parameters
 n_programs = 10
 program_start = 0
 program_musical_start = 6
@@ -21,55 +24,71 @@ pitch_samples = 3
 energy_samples = 1 
 energies_decay = 1-1./(10*samplerate/buffer_size)
 
-# Joystick setup
-pygame.init()
-pygame.joystick.init()
+# Joystick variables
 joysticks = []
-for i in range(0, pygame.joystick.get_count()):
-    joysticks.append(pygame.joystick.Joystick(i))
-    joysticks[-1].init()
-    print ("Detected joystick "),joysticks[-1].get_name(),"'"
 # 1, 2, 3, 4, L1, R1, L2, R2, Start, Select
 button_states = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 program_states = [0, 220, 40, 150, 0]
 
-# Radio setup
+# Radio variables
 radio = RF24(25, 0);
-pipes = 0xF0F0F0F0E1
 
-radio.begin()
-radio.setRetries(0,0)
-radio.disableCRC()
-radio.enableDynamicPayloads()
-radio.setAutoAck(False)
-radio.setChannel(90)
-radio.openWritingPipe(pipes)
-radio.stopListening()
+def init_bluetooth():
+    global bluetoothprocess
+    bluetoothprocess = subprocess.Popen(['bluetoothctl'], stdout = subprocess.PIPE)
 
-# Audio setup
-p = pyaudio.PyAudio()
-pyaudio_format = pyaudio.paFloat32
-n_channels = 1
-stream = p.open(format=pyaudio_format,
-                channels=n_channels,
-                rate=samplerate,
-                input=True,
-                frames_per_buffer=buffer_size)
+def init_joystick():
+    # Joystick setup
+    pygame.init()
+    pygame.joystick.init()
 
-# setup pitch
-pitch_o = aubio.pitch("default", win_s, hop_s, samplerate)
-pitch_o.set_unit("midi")
-pitch_o.set_tolerance(tolerance)
-pitch = 0
-pitchque = deque([], pitch_samples)
+    for i in range(0, pygame.joystick.get_count()):
+        joysticks.append(pygame.joystick.Joystick(i))
+        joysticks[-1].init()
+        print ("Detected joystick "),joysticks[-1].get_name(),"'"
 
-# setup filter
-f = aubio.filterbank(40, win_s) # documentation says 40 for mel coeffs
-f.set_mel_coeffs_slaney(samplerate)
-pv = aubio.pvoc(win_s, hop_s)
-energies_raw = np.zeros((energy_samples, n_energies))
-energies_max = np.zeros(n_energies)
-energies = np.zeros(n_energies)
+def init_radio():
+    # Radio setup
+    pipes = 0xF0F0F0F0E1
+    radio.begin()
+    radio.setRetries(0,0)
+    radio.disableCRC()
+    radio.enableDynamicPayloads()
+    radio.setAutoAck(False)
+    radio.setChannel(90)
+    radio.openWritingPipe(pipes)
+    radio.stopListening()
+
+def init_audio():
+     # Audio setup
+    global p, audio, stream
+    try:
+        p = pyaudio.PyAudio()
+        pyaudio_format = pyaudio.paFloat32
+        n_channels = 1
+        stream = p.open(format=pyaudio_format,
+                        channels=n_channels,
+                        rate=samplerate,
+                        input=True,
+                        frames_per_buffer=buffer_size)
+    except:
+       audio = False
+       return
+    
+
+    # setup pitch
+    pitch_o = aubio.pitch("default", win_s, hop_s, samplerate)
+    pitch_o.set_unit("midi")
+    pitch_o.set_tolerance(tolerance)
+    pitchque = deque([], pitch_samples)
+
+    # setup filter
+    f = aubio.filterbank(40, win_s) # documentation says 40 for mel coeffs
+    f.set_mel_coeffs_slaney(samplerate)
+    pv = aubio.pvoc(win_s, hop_s)
+    energies_raw = np.zeros((energy_samples, n_energies))
+    energies_max = np.zeros(n_energies)
+    energies = np.zeros(n_energies)
 
 def get_signal():
     global pitch, energies_raw, energies, energies_max
@@ -109,52 +128,135 @@ def send_arduino(message):
 def process_input():
     global program, button_states, program_states
     for event in pygame.event.get():
-        try:
-            button_states[event.button] = (event.type 
-                                            == pygame.JOYBUTTONDOWN)
-            if button_states[8]:
-                program += 1
-                if program>=(n_programs+program_start):
-                    program = program_start            
-            program_states[0] = int(button_states[9])
-        except:
-            print('Problem reading joystick')
-            
+        # print(event)
+        if(event.type == pygame.JOYBUTTONDOWN or event.type == pygame.JOYBUTTONUP):
+            if event.type == pygame.JOYBUTTONUP:
+                value = False
+            else:
+                value = True
+            if event.button in [6,7,8,9,10,11,12]:
+                button_states[8] = value
+            if event.button == 6:
+                program = 0
+            elif event.button == 7:
+                program = 1
+            elif event.button == 8:
+                program = (program+1)%program_musical_start
+            elif event.button == 9:
+                program = 2
+            elif event.button == 10:
+                program = 3
+            elif event.button == 11:
+                program = 4
+            elif event.button == 12:
+                program = 5
+            elif event.button in [4,5,13,14]:
+                button_states[9] = value
+            elif event.button == 0:
+                button_states[1] = value
+            elif event.button == 1:
+                button_states[3] = value
+            elif event.button == 2:
+                button_states[2] = value
+            elif event.button == 3:
+                button_states[0] = value
+
+        if event.type == pygame.JOYAXISMOTION:
+            # Mirror right button logic on same layout
+            if event.axis==3 and event.value < 0:
+                button_states[0] = True
+            if event.axis==3 and event.value > 0:
+                button_states[1] = True
+            if event.axis == 2 and event.value < 0:
+                button_states[2] = True
+            if event.axis == 2 and event.value > 0:
+                button_states[3] = True
+            if event.axis == 3 and event.value == 0:
+                button_states[1] = button_states[0] = False
+            if event.axis == 2 and event.value == 0:
+                button_states[2] = button_states[3] = False
+            # Joy hat logic
+            if event.axis==0 and event.value < 0:
+                button_states[5] = True
+            if event.axis==0 and event.value > 0:
+                button_states[4] = True
+            if event.axis == 1 and event.value < 0:
+                button_states[6] = True
+            if event.axis == 1 and event.value > 0:
+                button_states[7] = True
+            if event.axis == 0 and event.value == 0:
+                button_states[4] = button_states[5] = False
+            if event.axis == 1 and event.value == 0:
+                button_states[7] = button_states[6] = False          
+        if event.type == pygame.JOYHATMOTION:
+            if event.value == (-1, 0):
+                button_states[5] = True
+            if event.value == (1, 0):
+                button_states[4] = True
+            if event.value == (0, -1):
+                button_states[7] = True
+            if event.value == (0, 1):
+                button_states[6] = True
+            if event.value == (0, 0):
+                button_states[4] = button_states[5] = False
+                button_states[6] = button_states[7] = False
+                
     if button_states[0] and program_states[1]<255-button_delta:
         program_states[1] += button_delta
-    if button_states[2] and program_states[1]>=button_delta:
+    if button_states[1] and program_states[1]>=button_delta:
         program_states[1] -= button_delta
-    if button_states[1] and program_states[2]<255-button_delta:
+    if button_states[2] and program_states[2]<255-button_delta:
         program_states[2] += button_delta
     if button_states[3] and program_states[2]>=button_delta:
         program_states[2] -= button_delta
     if button_states[4] and program_states[3]<255-button_delta:
         program_states[3] += button_delta
-    if button_states[6] and program_states[3]>=button_delta:
+    if button_states[5] and program_states[3]>=button_delta:
         program_states[3] -= button_delta
-    if button_states[5] and program_states[4]<255-button_delta:
+    if button_states[6] and program_states[4]<255-button_delta:
         program_states[4] += button_delta
     if button_states[7] and program_states[4]>=button_delta:
         program_states[4] -= button_delta
-    
-while True:
-    try:
-        get_signal()
-        process_input()
-        message = [program, program_states[0], program_states[1],
-                    program_states[2], program_states[3], 
-                    program_states[4], 0]
-        if program >= program_musical_start:
-            for e in energies:
-                message.append(e)
-            message.append(pitch)
-                     
-        send_arduino(message)
-        
-    except KeyboardInterrupt:
-        print("*** Ctrl+C pressed, exiting")
-        break
+    if button_states[9]:
+        program_states[0] = 1
+    else:
+        program_states[0] = 0
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+def main_loop():
+    while True:
+        try:
+            if(audio):
+                get_signal()
+            else:
+                time.sleep(0.02)
+            process_input()
+            message = [program, program_states[0], program_states[1],
+                        program_states[2], program_states[3], 
+                        program_states[4], 0]
+            if program >= program_musical_start:
+                for e in energies:
+                    message.append(e)
+                message.append(pitch)
+                         
+            send_arduino(message)
+            
+        except KeyboardInterrupt:
+            print("*** Ctrl+C pressed, exiting")
+            break
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+if (__name__ == '__main__'):
+    init_bluetooth()
+    while len(joysticks) < 1:
+        init_joystick()
+        if len(joysticks)<1:
+            time.sleep(1)
+            pygame.joystick.quit()
+    init_radio()
+    init_audio()
+    if (not audio):
+        n_programs = program_musical_start
+    main_loop()
